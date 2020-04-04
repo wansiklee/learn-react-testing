4fc001a8e234faf8b4270a37d7e5c8ba9e25a894
wansiklee <dhkstlr2662@gmail.com>
2020-04-04 17:31:54 +0900

    Create DelayedToggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import TodoApp from "./TodoApp";
+import DelayedToggle from "./DelayedToggle";
 
 function App() {
-  return <TodoApp />;
+  return <DelayedToggle />;
 }
 
 export default App;
